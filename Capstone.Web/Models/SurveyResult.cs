8de1601d94238c56ff0ea2c8de88a12f5628c5bb
Joseph Laneve <joseph.c.laneve@gmail.com>
2019-07-10 09:34:39 -0400

    Completed the Survey Result Model

---
+++ Capstone.Web/Models/SurveyResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class SurveyResult
+    {
+        /// <summary>
+        /// ID for survey
+        /// </summary>
+        [Display(Name = "Survey ID")]
+        public int SurveyId { get; set; }
+
+        /// <summary>
+        /// Alpha Park code from Park table
+        /// </summary>
+        [Display(Name = "Park Code")]
+        public string ParkCode { get; set; }
+
+        /// <summary>
+        /// Email address of the survey taker
+        /// </summary>
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// State where the survey taker lives
+        /// </summary>
+        [Display(Name ="State")]
+        public string State { get; set; }
+
+        /// <summary>
+        /// Activity level of the survey taker
+        /// </summary>
+        [Display(Name ="Activity Level")]
+        public string ActivityLevel { get; set; }
+
+        /// <summary>
+        /// List of survey results from the database
+        /// </summary>
+        public IList<SurveyResult> AllSurveys { get; set; }
+    }
+}
