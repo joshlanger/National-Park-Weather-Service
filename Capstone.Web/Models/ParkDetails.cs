6d51da75940d71930b83764903992e873f8f41a0
Josh Langer <joshlanger@gmail.com>
2019-07-11 10:29:10 -0400

    detail page done except for temperature changes

--- Capstone.Web/Models/ParkDetails.cs
+++ Capstone.Web/Models/ParkDetails.cs
@@ -9,5 +9,52 @@ namespace Capstone.Web.Models
     {
         public Park DetailPark { get; set; }
         public IList<Weather> AllWeather { get; set; }
+        public Dictionary<string, string> WeatherAdvice = new Dictionary<string, string>()
+        {
+            {"snow", "Pack snow shoes." },
+            {"rain", "Pack rain gear and wear waterproof shoes." },
+            {"thunderstorms", "Seek shelter and avoid hiking on exposed ridges." },
+            {"sunny", "Pack sunblock." },
+            {"partly cloudy", "" },
+            {"cloudy", "" }
+            
+        };
+
+        public List<string> TemperatureAdvice = new List<string>()
+        {
+            {"Bring an extra gallon of water." },
+            {"Wear breathable layers." },
+            {"Danger! Exposure to temperatures this low can cause frost bite." }
+        };
+
+        //public string TemperatureAdice
+        //{
+        //    get
+        //    { if(this.AllWeather[0].High > 76)
+        //        {
+        //            return "Bring an extra gallon of water";
+                          
+        //        }
+        //    if(AllWeather[0].Low < 20)
+        //        {
+        //            return "Danger! Exposure to temperatures this low can cause frost bite";
+        //        }
+        //    if((AllWeather[0].High - AllWeather[0].Low) > 20)
+        //        {
+        //            return "Wear breathable layers";
+        //        }
+        //        return "";
+        //            }
+        //}
+
+        //public Dictionary<int, string> TemperatureAdvice = new Dictionary<int, string>()
+        //{
+        //    {76, "Bring an extra gallon of water" },
+        //    {21, "Wear breathable layers" },
+        //    {19, "Danger! Exposure to temperatures this low can cause frost bite" }
+        //};
+            
+        
+
     }
 }
