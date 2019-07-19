0b46cfbfef74e26e944323205d8fcc610142d216
Josh Langer <joshlanger@gmail.com>
2019-07-19 12:53:12 -0400

    first selenium test working

---
+++ Capstone.Web.Tests/Helper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Web.Tests
+{
+    public static class Helper
+    {
+        public static readonly string URL = "http://localhost:60349/";
+    }
+}
---
+++ Capstone.Web.Tests/ParkIntegrationWithoutPageObject.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace Capstone.Web.Tests
+{
+    [TestClass]
+    public class ParkIntegrationWithoutPageObject
+    {
+        private static IWebDriver webDriver;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            webDriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            webDriver.Navigate().GoToUrl(Helper.URL);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            webDriver.Close();
+        }
+
+        [TestMethod]
+        public void ElementsExistOnPage()
+        {
+            IWebElement grandCanyonImg = webDriver.FindElement(By.Id("GCNP"));
+            Assert.IsTrue(grandCanyonImg.Displayed);
+        }
+    }
+}
