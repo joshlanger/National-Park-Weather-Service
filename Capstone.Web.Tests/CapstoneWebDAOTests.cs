35fa1ac1920147dbd22b7d2c3ed3e4ee578a2401
Joseph Laneve <joseph.c.laneve@gmail.com>
2019-07-24 09:03:30 -0400

    working park dao test, not working survey dao test

--- Capstone.Web.Tests/CapstoneWebDAOTests.cs
+++ Capstone.Web.Tests/CapstoneWebDAOTests.cs
@@ -12,6 +12,7 @@ namespace Capstone.Web.Tests
     {
         protected string ConnectionString = "Server=.\\SQLEXPRESS;Database=NPGeek;Trusted_Connection=True";
         private TransactionScope transaction;
+        protected string NewParkId { get; private set; }
 
         [TestInitialize]
         public void Initialize()
@@ -23,11 +24,14 @@ namespace Capstone.Web.Tests
                 string cmd = "DELETE FROM weather; DELETE FROM survey_result; DELETE FROM park;";
                 SqlCommand command = new SqlCommand(cmd, conn);
                 command.ExecuteNonQuery();
-                cmd = "INSERT INTO park VALUES('ARCH', 'Arches National Park', 'Utah', 47389, 7000, 200, 100, 'Desert', 1929, 1284767, 'This place is great!', 'Joseph Laneve', 'A lot of sandstone arches here', 10, 500;";
+                cmd = "INSERT INTO park VALUES('ARCH', 'Cuyahoga Valley National Park', 'Ohio', 32832, 696, 125, 0, 'Woodland', 2000, 2189849, 'This place is great!', 'Joseph Laneve', 'A lot of sandstone arches here', 0, 390);select scope_identity();";
                 command = new SqlCommand(cmd, conn);
-                cmd = "INSERT INTO survey_result VALUES('ARCH', 'joseph.c.laneve@gmail.com', 'Pennsylvania', 'Active';";
+                NewParkId = Convert.ToString(command.ExecuteScalar());
+                cmd = $"INSERT INTO survey_result VALUES({NewParkId}, 'joseph.c.laneve@gmail.com', 'PA', 'Active');";
                 command = new SqlCommand(cmd, conn);
-                cmd = "INSERT INTO weather VALUES('ARCH', 1, 43, 73, 'partly cloudy';";
+                //cmd = "SELECT COUNT(*) as votes, survey_result.parkCode, parkName FROM survey_result JOIN park on survey_result.parkCode = park.parkCode GROUP BY parkName, survey_result.parkCode ORDER BY votes DESC, survey_result.parkCode;";
+                //command = new SqlCommand(cmd, conn);
+                cmd = $"INSERT INTO weather VALUES({NewParkId}, 6, 43, 73, 'partly cloudy');";
                 command = new SqlCommand(cmd, conn);
             }
         }
---
+++ Capstone.Web.Tests/SurveySqlDAOTests.cs
@@ -0,0 +1,24 @@
+﻿using Capstone.Web.DAO;
+using Capstone.Web.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Web.Tests
+{
+    //[TestClass]
+    //public class SurveySqlDAOTests : CapstoneWebDAOTests
+    //{
+    //    private SurveySqlDAO surveyDAO { get; set; }
+
+    //    [TestMethod]
+    //    public void GetSurveyResults_Should_Return_One_Survey()
+    //    {
+    //        surveyDAO = new SurveySqlDAO(ConnectionString);
+    //        IList<Survey> AllSurveys = surveyDAO.GetSurveyResults();
+    //        Assert.AreEqual(1, AllSurveys.Count);
+
+    //    }
+    //}
+}
